a02ff323d5ffb69e5f617ca9bc6ace9cc17e456c
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-06-10 00:24:45 +0500

    ok

--- src/server/server.ts
+++ src/server/server.ts
@@ -6,14 +6,14 @@ const PORT: number = Number(process.env.PORT) || 5000;
 import errorMiddleware from "../middleware/errorHandler.js";
 import userRoutes from "../routes/user.routes.js";
 import postRoutes from "../routes/post.routes.js";
-import swRouter from "../utils/swagger.js";
+import swRouter from "../utils/swagger.js"; 
 app.use(express.json());
 app.use('/docs', swRouter);
 app.use('/users', userRoutes);
 app.use('/post', postRoutes);
 app.use(errorMiddleware);
 // app.listen(PORT, () => console.log("Server listening on port" + PORT));
-
+  
 connectToDatabase().then(() => {
     app.listen(PORT, () => console.log("Server listening on port" + PORT));
 
